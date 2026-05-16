a1d3cd601a27cc235a0c778f5c6a83b26499b3c2
shrinivasa am <shrinivasmasalavada1@gmail.com>
2026-05-16 22:32:05 +0530

    Add files via upload

---
+++ Real-Time YOLO Detection.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import cv2
+
+# Load model
+model = YOLO("yolov8n.pt")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    results = model(frame)
+
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("YOLO Detection", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
